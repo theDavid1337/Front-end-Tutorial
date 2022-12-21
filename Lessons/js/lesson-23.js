a6891cc47837704b8f8ca8c546b6323ed230a5de
David <103839294+theDavid1337@users.noreply.github.com>
2022-12-21 17:52:09 +0500

    updating Lesson

---
+++ Lessons/js/lesson-23.js
@@ -0,0 +1,11 @@
+// TASK 1
+// let count = 3
+// let array = [1, 2, 3]
+// let helper_arr = []
+// for (let i = 0; i < count; i++) {
+//     array.forEach(elem => {
+//         helper_arr.push(elem)
+//     })
+// }
+
+// console.log(helper_arr);
